aa6e0ef58ba61c06beed85f935eed9d068f45daa
sashaij <o.zagorodniuk@outlook.com>
2023-12-04 15:47:46 +0200

    Add pseudo for an algorythm to find win sequence

--- script.js
+++ script.js
@@ -7,11 +7,11 @@
 
 //win sequences
 
-// 1-2-3, 4-5-6, 7-8-9 row
-// 9-8-7, 6-5-4, 3-2-1 row rev
-// 1-4-7, 2-5-8, 3-6-9 col 
-// 7-4-1, 8-5-2, 9-6-3 col rev
-// 3-5-7, 7-5-3, 1-5-9, 9-5-1 diag
+// 1, 2, 3, 4, 5, 6, 7, 8, 9 row
+// 9, 8, 7, 6, 5, 4, 3, 2, 1 row rev
+// 1, 4, 7, 2, 5, 8, 3, 6, 9 col 
+// 7, 4, 1, 8, 5, 2, 9, 6, 3 col rev
+// 3, 5, 7, 7, 5, 3, 1, 5, 9, 9, 5, 1 diag
 
 //9 moves total
 
@@ -51,6 +51,40 @@ function player (name) {
 
   return {playerName, getMoves, makeMove};
 }
+
+function defineWin () {
+  const winSeq = [
+   [1, 2, 3], [4, 5, 6], [7, 8, 9],
+   [9, 8, 7], [6, 5, 4], [3, 2, 1], 
+   [1, 4, 7], [2, 5, 8], [3, 6, 9],
+   [7, 4, 1], [8, 5, 2], [9, 6, 3], 
+   [3, 5, 7], [7, 5, 3], [1, 5, 9], [9, 5, 1] 
+  ];
+
+  /*
+  6, 15, 24, 12, 18, 
+   */
+
+  /* 
+  (i + (i+1) + (i+2))
+  sum = moves[i] + moves[i+1] + 
+  moves[i+2];
+  */
+
+  for (let i = 0; i < winSeq.length; i++) {
+    givenArray = winSeq[i];
+    let sum = 0;
+    for (let i = 0; i < winSeq[i].length; i++) {
+      sum += givenArray[i];
+    }
+    console.log(sum);
+    sum = 0;
+  } //loop that sums array values
+    //in an array of arrays
+
+}
+
+defineWin();
 
 const player1 = player('player1');
 const player2 = player('player2');
