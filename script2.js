4387c57152bcbc7bdefaac76a1fb2cfcec07bf0c
sashaij <o.zagorodniuk@outlook.com>
2023-12-19 09:10:04 +0200

    Add display who's turn it is

--- script2.js
+++ script2.js
@@ -48,49 +48,24 @@
 //game ends when total moves count
 //reaches 9 (while i<=9)
 
-/* 
-Certainly! Building a Tic Tac Toe game using factory functions is a great way to practice JavaScript and improve your coding skills. Here's a step-by-step guide to help you through the process:
 
-Board Representation:
+//tasks
+  //display who's turn it is now
+  //display when the game is over
+  //and display winner
+  //or draw
 
-Decide on a data structure to represent the Tic Tac Toe board. This could be a simple array, a 2D array, or an object. Each cell on the board should be identified by a unique index or coordinate.
+  //block the empty cells remaining
+  //when the game is over
 
-Player Factory Function:
 
-Create a factory function that generates player objects. Each player should have a name and a symbol (either "X" or "O").
-Game Board Module:
+  //new game button
 
-Implement a module or factory function to manage the game board. This should include functions to:
-Initialize the board.
-Display the current state of the board.
-Update the board with a player's move.
-Check for a win or a draw.
-Reset the board for a new game.
-Game Flow:
-
-Implement a function to manage the flow of the game.
-Alternately take turns between two players.
-Prompt the current player for a move.
-Validate the move (e.g., ensure the chosen cell is empty).
-Update the board with the move.
-Check for a win or a draw after each move.
-Display the result and end the game if a win or draw is detected.
-Allow the players to start a new game.
-User Input:
-
-Implement a function to handle user input. This could be through the console, a simple webpage, or any other interface you prefer.
-Refactoring and Optimization:
-
-Review your code and see if there are opportunities for refactoring. Consider separating concerns into different functions for better readability and maintainability.
-Testing:
-
-Test your game thoroughly to ensure it works as expected. Test various scenarios, including wins, draws, and different player moves.
-Documentation:
-
-Add comments or documentation to your code to explain how each part works. This is especially important if you plan to share your code with others or review it in the future.
-Remember, the key to learning is not just in writing code but also in understanding the logic behind it. Take your time at each step, and don't hesitate to ask questions if you get stuck. Good luck, and have fun building your Tic Tac Toe game!
-*/
-
+    //clear board
+     //delete marks 
+     //make a loop that picks each
+     //element (how?) 
+     //and removes (.remove();) it
 
 //gameboard
 
@@ -140,10 +115,8 @@ function setMark () {
           </svg>
           `
         } 
-        /* buttonAttribute = Number(gridButton[i].getAttribute('data-index')) */
+          //trigger
           game.makeMove(buttonAttribute);
-        //* here, but how to
-        //  display number correctly?
       }
 
         const getMark = (e) => {
@@ -160,11 +133,7 @@ function setMark () {
         )
         gridButton[i].addEventListener('click',   placeMark
       );
-      
-      /* gridButton[i].addEventListener('click', getMark(i)) */
-        //where to put makemove (without ev list)
-        //to display number right and not
-        //and not trigger func immidetly
+     
       
       }
 
@@ -174,39 +143,26 @@ function setMark () {
   return {placeMark, getMark}
 }
 
-
-
-
-
-function whichMark (player) {
-  const playerName = player.name;
-  let targetImg = document.querySelector(`#${a}`);
-  const selctMark = () => {
-    if (playerName === 'Player One') {
-      
-    }
+function statusDisplay () {
+  const showStatus = () => {
+    const gameDisplay = document.querySelector('.display');
+  const currentPlayer = game.getActivePlayer().name;
+  gameDisplay.innerHTML = `It's ${currentPlayer}'s turn now. Make a turn.`
   }
+  return {showStatus};
 }
 
+const displayStatus = statusDisplay();
+displayStatus.showStatus();
+
 function Gameboard () {
-    const rows = 3;
-    const columns = 3;
     let board = [];
-
-
-    /* for (let i = 0; i < rows; i++) {
-      board [i] = [];
-      for (let j = 0; j <columns; j++) {
-        board[i].push(Cell());
-      }
-    } */
 
     const fillBoard = (num) => board.push(num);
 
     //function that defines if 
     //moves quantity is exceeded and 
     //round is over
-
     const ifEnd = () => {
       if (board.length >= 9) {
         console.log('Game over');
@@ -215,8 +171,6 @@ function Gameboard () {
 
     const getBoard = () => board;
     const emptyBoard = () => board = [];
-
-    //function that counts overall turns
 
     return {getBoard, fillBoard, emptyBoard, ifEnd};
 }
@@ -237,21 +191,21 @@ const whoWin = function (arr) {
   let testArr = arr;
   let arrBool;
 
-
-      const arrComparison = (firstArr, secondArr) => {
-  firstArr.sort();
-  secondArr.sort();
-  return firstArr + "" == secondArr + "";
-  }
+      
+  const arrComparison = (firstArr, secondArr)=> {
+    firstArr.sort();
+    secondArr.sort();
+     return firstArr + "" == secondArr + "";
+   }
 
   const ifWinner = () => {
-    if (testArr.length < 3) {
-      return
-  }
+      if (testArr.length < 3) {
+        return
+      }
       for(let i = 0; i < sequences.length; i++){
         arrBool = arrComparison(sequences[i], testArr);
-        if (arrBool === true) {
-            console.log(`${game.getActivePlayer().name} is a winner.`);
+       if (arrBool === true) {
+         console.log(`${game.getActivePlayer().name} is a winner.`);
       }
     }
   }
@@ -358,12 +312,10 @@ function GameFlow (
   const players = [
     {
       name: playerOneName,
-      mark: 'x',
       moves: [],
     },
     {
       name: playerTwoName,
-      mark: 'o',
       moves: [],
     },
   ];
@@ -406,14 +358,13 @@ function GameFlow (
     newTest = whoWin(game.getPlayerMoves());
     newTest.ifWinner();
     switchPlayer();
+    displayStatus.showStatus();
     printTurn();
   } 
   
 
   return {getPlayerName, getActivePlayer, makeMove, getPlayerMoves, printTurn, receivePlayersMoves, resetAll}
 }
-
-
 
 game.printTurn();
 const plMoves = game;
