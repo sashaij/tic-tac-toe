161b0f78a1d0321fc85c737e40ff4035a16b6e45
sashaij <o.zagorodniuk@outlook.com>
2023-12-23 14:50:19 +0200

    Rewrite-reorganize code

---
+++ script3.js
@@ -0,0 +1,122 @@
+//gameboard
+   
+function Gameboard () {
+    let board = [];
+
+    //win combinations
+    const sequences = [  
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],  
+      ]
+
+    //test if active player
+    //has win combination
+    //returns true if match is reached 
+    const winnerTest = (firstArr, secondArr) => {
+        for (let i = 0; i < secondArr.length; i++) {
+            firstArr.sort();
+            secondArr[i].sort();
+            if (firstArr + "" == secondArr[i] + "") return true;
+        }
+    }
+
+
+    const fillBoard = (num) => {
+        //stop execution if board is 
+        //the entire board is filled
+        if (board.length >= 9) return;
+        board.push(num);
+    } 
+
+    //method to get win combinations;
+    const getSequences = () => sequences;
+    //method of getting the board;
+    const getBoard = () => board;
+    //method of emptying board;
+    const emptyBoard = () => board = [];
+
+    return {getBoard, getSequences, fillBoard, emptyBoard, winnerTest};
+}
+
+
+//game controller
+function GameFlow (
+    playerOneName = 'Player One',
+    playerTwoName = 'Player Two'
+  ) {
+    
+    const board = Gameboard();
+
+    const players = [
+      {
+        name: playerOneName,
+        moves: [],
+      },
+      {
+        name: playerTwoName,
+        moves: [],
+      },
+    ];
+
+    const gameBoard = Gameboard();
+    const uInterface = UserInterface();
+  
+    let activePlayer = players[0];
+  
+    const switchPlayer = () => {
+      activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+  
+    const getActivePlayer = () => activePlayer;
+    
+    //add moves to the moves array 
+    //and switch player
+    
+    const resetAll = () => { //reset all to default
+      activePlayer = players[0];
+      players[0].moves = 
+      players[1].moves = [];
+      newGameBoard.emptyBoard();
+    }
+    
+    const printTurn = () => console.log(`It's ${activePlayer.name}'s move now. Make a move`);
+    const getPlayerName = () => activePlayer.name;
+    const getPlayerMoves = () => activePlayer.moves;
+    
+    const makeMove = (move) => {
+        //add cell index to 
+        //active pl array
+      activePlayer.moves.push(Number(move));
+      //add cell index to board array
+      board.fillBoard(Number(move));
+      console.log('overall moves: ' + board.getBoard());
+      //if length of moves exceeds 3
+      //delete first item in the array
+      if (activePlayer.moves.length > 3) {   
+        activePlayer.moves.shift();   
+      }  
+      
+      //check if there is a winner
+      if (gameBoard.winnerTest(getPlayerMoves(), board.getSequences())) {
+        const boardDisplay = uInterface.getDisplay();
+        boardDisplay.innerHTML = `${getPlayerName} is a winner. Game over.`
+      }
+    } 
+    
+  
+    return {getPlayerName, getActivePlayer, makeMove, getPlayerMoves, printTurn, resetAll}
+  }
+
+//UI
+function UserInterface () {
+    const display = document.querySelector('.display');
+
+    const getDisplay = () => display;
+
+    return {getDisplay};
+}
