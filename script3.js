7bf5377f902bd55fe7b3bcfbd7f7a0eca82d1aa6
sashaij <o.zagorodniuk@outlook.com>
2023-12-24 10:32:34 +0200

    Add method for putting mark

--- script3.js
+++ script3.js
@@ -1,5 +1,6 @@
 const game = GameFlow();
-
+const userInterface = UserInterface();
+userInterface.placeMark();
 //gameboard
    
 function Gameboard () {
@@ -137,9 +138,71 @@ function GameFlow (
 //UI
 function UserInterface () {
     const display = document.querySelector('.display');
+    //select all grid button elements
     const gridButton = document.querySelectorAll('.grid-button');
+    let buttonAttribute;
 
     const getDisplay = () => display;
 
-    return {getDisplay};
+    const placeMark = (e) => {
+              
+      
+              if(!e.target.classList.contains('grid-button')){
+                return;
+              } 
+            
+              let actPlayer = game.getPlayerName();
+              //console.log('this works');
+              //console.log(e.target.closest('div.cell-container'));
+            
+              const container = e.target.closest('div.cell-container');
+            
+              if (actPlayer === "Player One") {
+                  container.innerHTML = 
+              `
+                <svg fill="#9f43d0" width="100px" height="100px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" stroke="#9f43d0">
+            
+                <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+                
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+                
+                <g id="SVGRepo_iconCarrier"> <path d="M18.8,16l5.5-5.5c0.8-0.8,0.8-2,0-2.8l0,0C24,7.3,23.5,7,23,7c-0.5,0-1,0.2-1.4,0.6L16,13.2l-5.5-5.5 c-0.8-0.8-2.1-0.8-2.8,0C7.3,8,7,8.5,7,9.1s0.2,1,0.6,1.4l5.5,5.5l-5.5,5.5C7.3,21.9,7,22.4,7,23c0,0.5,0.2,1,0.6,1.4 C8,24.8,8.5,25,9,25c0.5,0,1-0.2,1.4-0.6l5.5-5.5l5.5,5.5c0.8,0.8,2.1,0.8,2.8,0c0.8-0.8,0.8-2.1,0-2.8L18.8,16z"/> </g>
+                
+                </svg>
+                                  `
+              } else {
+                container.innerHTML = `
+                <svg fill="#80e77e" width="90px" height="90px" viewBox="0 0 34 34" version="1.1" xmlns="http://www.w3.org/2000/svg" stroke="#80e77e">
+            
+                <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+                
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+                
+                <g id="SVGRepo_iconCarrier"> <title>circle</title> <path d="M0 16q0 3.264 1.28 6.208t3.392 5.12 5.12 3.424 6.208 1.248 6.208-1.248 5.12-3.424 3.392-5.12 1.28-6.208-1.28-6.208-3.392-5.12-5.088-3.392-6.24-1.28q-3.264 0-6.208 1.28t-5.12 3.392-3.392 5.12-1.28 6.208zM4 16q0-3.264 1.6-6.016t4.384-4.352 6.016-1.632 6.016 1.632 4.384 4.352 1.6 6.016-1.6 6.048-4.384 4.352-6.016 1.6-6.016-1.6-4.384-4.352-1.6-6.048z"/> </g>
+                
+                </svg>
+                `
+              } 
+                //trigger
+                game.makeMove(buttonAttribute);
+            }
+      
+              const getMark = (e) => {
+                let element = e.target;
+                let index = element.getAttribute('data-index');
+                buttonAttribute = index;
+            }
+      
+            
+      
+            for (i = 0; i < gridButton.length; i++){
+              gridButton[i].addEventListener('click',
+              getMark
+              )
+              gridButton[i].addEventListener('click',   placeMark
+            );
+        }
+      
+
+    return {getDisplay, placeMark};
 }
